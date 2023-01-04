55b9b9c187609757815d50e4e2b6a28a3d94a342
ChenZhigang <chenzg@tiens.com>
2023-01-04 14:02:08 +0800

    index adjusted

---
+++ src/main/java/com/euclidolap/olapweb/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.euclidolap.olapweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "page/index";
+    }
+
+}
