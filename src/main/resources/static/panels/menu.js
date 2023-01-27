0518f9683cd1dce50f9161309921da00f40d7505
ChenZhigang <chenzg@tiens.com>
2023-01-27 11:32:31 +0800

    update

--- src/main/resources/static/panels/menu.js
+++ src/main/resources/static/panels/menu.js
@@ -6,24 +6,33 @@ function MenuController() {
     this.$mdxTer;
 
     this.doInit = function() {
+
         this.$mdxTer = $('#' + this.divId + '_mdxTer');
-        this.$mdxTer.click(this, function(event) {
-            var ctl = event.data;
-            ctl.$mdxTer.css({
-                "background-color": "#39495e",
-                "color": "#ffffff"
-            });
-            console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>> click [MDX Terminal]");
-            if (ctl.$mdxTer.attr("data-exist") != "Y") {
-                var workAreaDivId = WW.findController("stage").createWorkArea();
-                WW.findFactory("work/mdxTerminal").newController().init(workAreaDivId);
-                ctl.$mdxTer.attr("data-exist", "Y");
-            }
-//            else {
-//                this.workAreaDivId = WW.findController("stage").createWorkArea();
-//                WW.findFactory("work/mdxTerminal").newController().init(this.workAreaDivId);
+
+//        this.$mdxTer.click(this, function(event) {
+//            var ctl = event.data;
+//            ctl.$mdxTer.css({
+//                "background-color": "#39495e",
+//                "color": "#ffffff"
+//            });
+//            if (ctl.$mdxTer.attr("data-exist") != "Y") {
+//                var workAreaDivId = WW.findController("stage").createWorkArea();
+//                WW.findFactory("work/mdxTerminal").newController().init(workAreaDivId);
+//                ctl.$mdxTer.attr("data-exist", "Y");
 //            }
-        });
+//        });
+
+        (function(_ctl_) {
+             _ctl_.$mdxTer.css({
+                 "background-color": "#39495e",
+                 "color": "#ffffff"
+             });
+             if (_ctl_.$mdxTer.attr("data-exist") != "Y") {
+                 var workAreaDivId = WW.findController("stage").createWorkArea();
+                 WW.findFactory("work/mdxTerminal").newController().init(workAreaDivId);
+                 _ctl_.$mdxTer.attr("data-exist", "Y");
+             }
+         })(this);
     };
 
     this.workAreaDivId = null;
